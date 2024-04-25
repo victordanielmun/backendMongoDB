cfc644754e40c6a54bff9603b33bccac9190110f
victordanielmun <31445435+victordanielmun@users.noreply.github.com>
2024-04-25 01:50:42 -0500

    fix port
    add PORT variable to index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import app from "./app.js";
 import { connectDB } from "./db.js";
+const PORT = process.env.PORT || 5000;
 
 connectDB();
-app.listen(3001, () => console.log("Server started on port 3001s"));
+app.listen(PORT, () => console.log("Server started on port 5000"));
