752db57879111e4283fe57cd7e7dfbe03adac115
victordanielmun <31445435+victordanielmun@users.noreply.github.com>
2024-04-25 02:02:55 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,1 @@
- export const TOKEN_SECRET = "secretkey";
- export const URI = process.env.MONGO_URI || "mongodb+srv://victordanielmun:89rCEclh9JEqboNK@cluster0.rwyrfvn.mongodb.net/"
+ export const TOKEN_SECRET = "secretkey"
